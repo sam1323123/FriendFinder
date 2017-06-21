55e3aafcef17aac672c7a4c2af6fae9d2e4d5a29
Samuel Lee <samuell1@andrew.cmu.edu>
2017-06-21 02:21:29 -0500

    DEV: Added main.storyboard back

---
+++ FriendFinder/FriendFinder/ButtonController.swift
@@ -0,0 +1,35 @@
+//
+//  ButtonController.swift
+//  FriendFinder
+//
+//  Created by Samuel Lee on 6/21/17.
+//  Copyright © 2017 Samuel Lee and Avishek Ganguli. All rights reserved.
+//
+
+import UIKit
+
+class ButtonController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
